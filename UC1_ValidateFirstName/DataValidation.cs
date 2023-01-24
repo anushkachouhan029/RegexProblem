b75de4756069a0c2e3fcabfcbdb8fffe592f161f
anushkachouhan029 <anushkachouhan029>
2023-01-24 23:20:36 +0530

    added validate email

--- UC1_ValidateFirstName/DataValidation.cs
+++ UC1_ValidateFirstName/DataValidation.cs
@@ -11,6 +11,7 @@ namespace Regex01
     {
         public Regex First_Name_Regex = new Regex(@"^[A-Z]{1}[A-Za-z]{2,}?$");
         public Regex Last_Name_Regex = new Regex(@"^[A-Z]{1}[A-Za-z]{2,}?$");
+        public Regex Email_Regex = new Regex("^[A-Za-z]{3,}([.][A-Za-z]{3,})?[@][A-Za-z]{2,}[.][A-Za-z]{2,}([.][A-Za-z]{2})?$");
         public void ValidateFirstName()
         {
             Console.Write("Enter First Name : ");
@@ -39,6 +40,16 @@ namespace Regex01
                 Console.WriteLine("The Last Name Is Not Valid");
             }
         }
+        public void ValidateEmail(string email)
+        {
+            if (Email_Regex.IsMatch(email))
+            {
+                Console.WriteLine("Email Is Valid ");
+            }
+            else
+            {
+                Console.WriteLine("Email Is Not Valid ");
+            }
+        }
     }
-
 }
